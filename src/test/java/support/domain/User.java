c00646a268ddb88b2083e187dbd5db4a48630a8f
Rafael Lima <rlima@thoughtworks.com>
2019-02-28 10:13:25 -0300

    Remove the jsonproperty annotation

--- src/test/java/support/domain/User.java
+++ src/test/java/support/domain/User.java
@@ -1,11 +1,9 @@
 package support.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class User {
 
-    @JsonProperty("nome")
     private String nome;
 }
